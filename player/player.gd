82c6005ec27157bb83e58b9915413cd8221674fc
Markus Philipp <m.philipp1991@web.de>
2023-10-07 12:45:31 +0200

    Add spawn logic

--- player/player.gd
+++ player/player.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
 @export var player_color: Color
-@export var controller_index: int 
+@export_range(-1, 3) var controller_index: int 
 @onready var polygon: Polygon2D = $Polygon2D
 
 const BASE_SPEED = 500
 
+func init(device_id: int, color: Color):
+	player_color = color
+	controller_index = device_id
+	
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	polygon.color = player_color
-	MultiplayerInput.reset()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
---
+++ player/player_spawner.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+
+const PLAYER_COLORS = [
+	Color(0.98, 0, 0.33),
+	Color(0, 0.98, 0.1),
+	Color(0, 0.5, 0.5),
+	Color(0.65, 0.5, 0)
+]
+const MAX_PLAYERS = 4
+var players = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var devices = [-1] + Input.get_connected_joypads()
+	print(devices)
+	for device_id in devices:
+		spawn_player(device_id)
+
+
+func spawn_player(device_id: int) -> void:
+	var id: int = players.size()
+	var player_node: CharacterBody2D = load("res://player/player.tscn").instantiate()
+	players[id] = {
+		"device_id": device_id,
+		"color":  PLAYER_COLORS[id],
+		"node": player_node
+	}
+	player_node.init(device_id, players[id]["color"])
+	# randomize spawn location
+	player_node.position = Vector2(randf_range(-100, 100), randf_range(-100, 100))
+	
+	add_child(player_node)
+	print("added player: " + str(id))
+	print_tree()
+	
+	
+	
